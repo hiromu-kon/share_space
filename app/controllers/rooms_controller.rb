4cfa7c22e5268e149f11e953a133cfc040305bae
kondou <hiro.0405soccer@icloud.com>
2021-03-16 23:11:49 +0900

    add：messagesコントローラーの作成

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,31 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = current_host_user.rooms.includes(:messages).order("messages.created_at desc")
+  end
+
+  def create
+    @room = Room.create
+    @joinCurrentUser = Entry.create(host_user_id: current_host_user.id, room_id: @room.id)
+    @joinUser = Entry.create(join_room_params)
+    @first_message = @room.messages.create(host_user_id: current_host_user.id, message: "初めまして！")
+    redirect_to room_path(@room.id)
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    if Entry.where(host_user_id: current_host_user.id, room_id: @room.id).present?
+      @messages = @room.messages.includes(:host_user).order("created_at asc")
+      @message = Message.new
+      @entries = @room.entries
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def join_room_params
+    params.require(:entry).permit(:host_user_id, :room_id).merge(room_id: @room.id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'messages/create'
+  get 'messages/edit'
+  get 'messages/update'
+  get 'messages/destroy'
+  get 'rooms/index'
+  get 'rooms/create'
+  get 'rooms/show'
   devise_for :call_center_users, controllers: {
     sessions: 'call_center_users/sessions',
     registrations: 'call_center_users/registrations',
@@ -17,5 +24,7 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+  resources :rooms, only: [:index, :create, :show]
+  resources :messages, only: [:create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
