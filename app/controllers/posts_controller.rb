d0b396cee4d5954be534306e489b74387f1c7eee
kondou <hiro.0405soccer@icloud.com>
2021-03-18 22:05:03 +0900

    change：Post詳細からもメッセージページに飛べるように変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,26 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
       Pv.create(ip: request.remote_ip)
     end
+
+    host_user = @post.host_user
+    if host_user_signed_in?
+      @currentHostUserEntry = Entry.where(host_user_id: current_host_user.id)
+      @hostUserEntry = Entry.where(host_user_id: host_user.id)
+      unless host_user.id == current_host_user.id
+        @currentHostUserEntry.each do |cu|
+          @hostUserEntry.each do |u|
+            if cu.room_id == u.room_id
+              @haveRoom = true
+              @roomId = cu.room_id
+            end
+          end
+        end
+        unless @haveRoom
+          @room = Room.new
+          @entry = Entry.new
+        end
+      end
+    end
   end
 
   def edit
