6278f9255b7e236bc42b5824957be9d1303b1795
kondou <hiro.0405soccer@icloud.com>
2021-03-21 17:26:14 +0900

    change：ボタンのデザイン変更

--- app/javascript/packs/swiper.js
+++ app/javascript/packs/swiper.js
@@ -1,14 +1,13 @@
 document.addEventListener("turbolinks:load", function () {
   var mySwiper = new Swiper('.swiper-container', {
-    effect: 'coverflow',
     loop: true,
-    speed: 2000,
-    autoplay: {
-      delay: 3000,
-      stopOnLastSlide: false,
-      disableOnInteraction: false,
-      reverseDirection: false
-    },
+    speed: 1000,
+    // autoplay: {
+    //   delay: 5000,
+    //   stopOnLastSlide: false,
+    //   disableOnInteraction: false,
+    //   reverseDirection: false
+    // },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
