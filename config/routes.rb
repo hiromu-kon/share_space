f6f4a5a40d5bd19bf1330dad8a2997675e0857e4
kondou <hiro.0405soccer@icloud.com>
2021-03-31 14:12:34 +0900

    delete：不要なviewファイルの削除

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/terms', to: 'static_pages#terms'
   get '/privacy', to: 'static_pages#privacy'
-  resources :call_center_users, only: :show
   resources :users, only: :show do
     resources :bookmarks, only: [:index]
   end
