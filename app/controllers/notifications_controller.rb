b9f94464e8d8da2bedad10ab3b5ffe5429c8c177
kondou <hiro.0405soccer@icloud.com>
2021-04-07 12:03:48 +0900

    add：Google map表示に必要なGemを追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @notifications = current_user.passive_notifications.includes(:visitor, :visited, :comment, :message)
     .page(params[:page]).per(10).where.not(visitor_id: current_user.id)
